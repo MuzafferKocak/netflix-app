2968321e9a91fe37e22bb533f6184dc18c67a8fb
Mahir <mkojak75@gmail.com>
2024-10-14 01:23:27 +0200

    herosection

--- src/app/(private)/movies/page.jsx
+++ src/app/(private)/movies/page.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { getMovies } from "@/helpers/movieFunctions";
+import React from "react";
+import HeroSection from "./components/HeroSection";
 
-const Movies = () => {
+const Movies = async () => {
+  const movies = await getMovies("now_playing");
+  console.log(movies);
   return (
-    <div>Movies</div>
-  )
-}
+    <div>
+      <HeroSection
+        id={movies[0].id}
+        title={movies[0]?.title}
+        overview={movies[0]?.overview}
+      />
+    </div>
+  );
+};
 
-export default Movies
+export default Movies;
---
+++ src/helpers/movieFunctions.jsx
@@ -0,0 +1,23 @@
+const API_KEY = process.env.TMDB_KEY;
+
+export const getMovies = async (type) => {
+  const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
+
+  const res = await fetch(URL);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const { results } = await res.json();
+  return results;
+};
+
+export const getVideoKey = async (movieId) => {
+  const videoUrl = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${API_KEY}`;
+
+  const res = await fetch(videoUrl);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  return data?.results[0]?.key;
+};
