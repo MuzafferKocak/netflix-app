3bd8a8266dd934a7be309ce799aa2d7718a86cb7
Mahir <mkojak75@gmail.com>
2024-10-14 02:15:22 +0200

    next config

--- src/app/(private)/movies/components/MovieCard.jsx
+++ src/app/(private)/movies/components/MovieCard.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+"use client"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCard = ({poster_path, vote_average}) => {
+const MovieCard = ({ poster_path, vote_average, id }) => {
+  const router = useRouter();
   return (
-    <div className='w-40 h-[240px] relative cursor-pointer'>
-    <img src={poster_path ? IMG_API + poster_path : defaultImage} alt="" />
+    <div
+      className="w-40 h-[240px] relative cursor-pointer"
+      onClick={() => router.push("/movies/" + id)}
+    >
+      <Image src={poster_path ? IMG_API + poster_path : defaultImage} alt="" 
+      width={160}
+      height={240} />
 
+      <span className="absolute bottom-1 right-1 text-white font-semibold z-10">
+        {vote_average.toFixed(1)}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
--- src/app/(private)/movies/components/MovieList.jsx
+++ src/app/(private)/movies/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({movies}) => {
   return (
-    <div className="grid grid-flow-col gap-2">
+    <div className="grid grid-flow-col gap-2 overflow-x-scroll">
       {movies.map((movie) => (
         <MovieCard key={movie.id} {...movie} />
       ))}
